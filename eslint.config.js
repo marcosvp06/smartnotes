ad7f7792d007532b2060842a80ab26c521d502cc
Marcos Paulo <marcos.pedrosa@icomp.ufam.edu.br>
2026-08-31 10:32:18 -0300

    Fix: Consertando o eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+  { files: ["*src/**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended, 
+  {rules : {"camelcase": "error", "capitalized-comments" : "error"}}
 ]);
